3ce24aa9e753e82bfe36bca6284e1e94bbc7e95b
Ruslan Gilvanov <rus8-2002@mail.ru>
2019-02-15 11:58:41 +0300

    Json import done

--- pylvlred.py
+++ pylvlred.py
@@ -6,7 +6,8 @@ import pygame.image as pim
 cell_X = int(input())
 if cell_X < 16:
     cell_X = 16
-cell_Y = int(input()) + 12
+H = int(input())
+cell_Y = H + 12
 cell_size = 16
 pygame.init()
 size = int(cell_X) * cell_size, int(cell_Y) * cell_size
@@ -32,7 +33,7 @@ items = []
 for i in range(16):
     for j in range(8):
         items.append(load_image(j, i, 1))
-
+player = load_image(0, 0, 2)
 class Field:
     # создание поля
     def __init__(self, width, height):
@@ -42,8 +43,9 @@ class Field:
         self.brush = 0
         self.item_brush = 0
 
-        self.board = [[-1] * width for _ in range(height)]
-        self.item_board = [[-1] * width for _ in range(height)]
+        self.board = [[-1 for _ in range(self.width)] for _ in range(height)]
+        print(self.board[0][0])
+        self.item_board = [[-1 for _ in range(width)] for _ in range(height)]
         a = -16
         for j in range(height - 12, height - 8):
             a += 16
@@ -58,7 +60,7 @@ class Field:
         self.cell_size = 16
 
     def give(self):
-        return self.board
+        return self.board, self.item_board
 
     def set_view(self, left, top, cell_size):
         self.left = left
@@ -77,15 +79,16 @@ class Field:
                                          self.cell_size
                                      ), 1
                                      )
+                if self.board[y][x] > -1 and self.board[y][x] < 48:
+                    screen.blit(blocks[self.board[y][x]], (x * self.cell_size + self.left, y * self.cell_size + self.top))
         for y in range(self.height - 9):
             for x in range(16):
-
-                if self.board[y][x] > -1 and self.board[y][x] < 64:
+                if self.board[y][x] > -1 and self.board[y][x] < 48:
                     screen.blit(blocks[self.board[y][x]], (x * self.cell_size + self.left, y * self.cell_size + self.top))
         for y in range(self.height - 12):
-            for x in range(16):
+            for x in range(self.width):
                 if self.item_board[y][x] > -1 and self.item_board[y][x] < 128:
-                    screen.blit(items[self.item_board[y][x]],(x * self.cell_size + self.left, y * self.cell_size + self.top))
+                    screen.blit(items[self.item_board[y][x]], (x * self.cell_size + self.left, y * self.cell_size + self.top))
         for y in range(self.height - 9, self.height):
             for x in range(16):
                 if self.item_board[y][x] > -1 and self.item_board[y][x] < 128:
@@ -124,14 +127,14 @@ class Levelmaker(Field):
         super().__init__(width, height)
 
 
-board = Field(int(cell_X), cell_Y)
+board = Field(cell_X, cell_Y)
 
 time_on = False
 ticks = 0
 board.render()
 running = True
 while running:
-    screen.fill((0, 0, 0))
+    screen.fill((50, 50, 50))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -145,14 +148,25 @@ while running:
     clock.tick(50)
     ticks += 1
 pygame.quit()
-plate = board.give()
+plate, item_plate = board.give()
 codificator  = {'-1': None, '0': None, '1': 'brick_floor', '2': 'scnd_brick_floor', '3': 'hole', '4': 'brick_wall', '5': 'wood_door', '6': 'opened_door', '7': 'stair_up', '8': 'stair_down',
                 '9': 'redstone', '10': 'locked_wood_door', '11': 'monolit', '12': 'tube', '13':'bamboo', '14': 'wood_floor', '15': 'thrd_brick_floor', '16': 'brick_wall',
                 '17': 'green_plate', '18': 'brick_floor', '19': 'red_plate', '20': 'brick_floor', '21': 'orange_plate', '22': 'brick_floor', '23': 'used_spikes', '24': 'moody_floor',
                 '25': 'steel_door', '26': 'steel_locked_door', '27': 'purpure_plate', '28': 'brick_floor', '29': 'tip', '30': 'red_plate', '31': 'brick_floor', '32': 'blue_plate',
                 '33': 'brick_floor', '34': 'scnd_hole', '35': 'brick_monument', '36': 'wood_monument', '37': 'moody_plate', '38': 'brick_floor', '39': 'white_plate', '40': 'brick_floor',
-                '41': 'book_shelf', '42': 'kolodecz', '43': 'pit', '44': 'pillar', '45': 'moody_wall', '46': 'moody_mono_wall', '47': 'brick_floor'}
+                '41': 'book_shelf', '42': 'kolodecz', '43': 'pit', '44': 'pillar', '45': 'moody_wall', '46': 'moody_mono_wall', '47': 'secret_floor'}
+item_codificator = {0: 'bones', 1: 'christ', 2: 'sworld', 3: 'magic_wend', 4: 'bread', 5: 'weapon_shadow', 6: 'armour_shadow', 7: 'ring_shadow',
+                    8: 'bone_key', 9: 'silver_key', 10: 'gold_key', 11: 'standart_chest', 12: 'key_chest', 13: 'rip_stone', 14: 'coins', 15: 'battle_star',
+                    16: 'knuckle', 17: 'stick', 18: 'mace', 19: 'small_sworld', 20: 'middle_sworld', 21: 'big_sworld', 22: 'battle_axe', 23: 'battle_hammer',
+                    24: 'fabric_armour', 25: 'leather_armour', 26: 'chain_armor', 27: 'gold_armour', 28: 'silver_armour', 29: 'small_spear', 30: 'big_spear', 31: 'arrow',
+                    32: 'glass_ring', 33: 'dimond_ring', 34: 'topaz_ring', 35: 'ruby_ring', 36: ''}
+
 ark = {'next level': input(), 'start_position': {}, "width": cell_X, "height": cell_Y, "board": []}
+for y in range(cell_Y - 12):
+   for x in range(cell_X):
+        cell = {}
+        cell['block'] = codificator[board]
+
 
 
 
